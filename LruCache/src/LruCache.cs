7931d3e80f2d6738453e81a255eee998ae4c6695
vieirandre <andre.vieira@outlook.com>
2025-01-25 00:48:13 -0300

    Draft interface implementation

---
+++ LruCache/src/LruCache.cs
@@ -0,0 +1,14 @@
+﻿namespace LruCache;
+
+public class LruCache<TKey, TValue> : ILruCache<TKey, TValue>
+{
+    public TValue Get(TKey key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Put(TKey key, TValue value)
+    {
+        throw new NotImplementedException();
+    }
+}
