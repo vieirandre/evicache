b667805bcee63b521e6fa6ad6d97b3c6f76ced54
vieirandre <andre.vieira@outlook.com>
2025-01-25 00:53:38 -0300

    Set ctor & capacity field

--- LruCache/src/LruCache.cs
+++ LruCache/src/LruCache.cs
@@ -2,6 +2,15 @@
 
 public class LruCache<TKey, TValue> : ILruCache<TKey, TValue>
 {
+    private readonly int _capacity;
+
+    public LruCache(int capacity)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(capacity);
+
+        _capacity = capacity;
+    }
+
     public TValue Get(TKey key)
     {
         throw new NotImplementedException();
