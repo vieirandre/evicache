3a666709cd55d71b09c67908d17d3f7729cb6eaf
vieirandre <andre.vieira@outlook.com>
2025-01-25 00:28:11 -0300

    Make key readonly (& use primary ctor)

--- LruCache/src/Models/CacheItem.cs
+++ LruCache/src/Models/CacheItem.cs
@@ -1,13 +1,7 @@
 ﻿namespace LruCache.Models;
 
-public class CacheItem<TKey, TValue>
+public class CacheItem<TKey, TValue>(TKey key, TValue value)
 {
-    public TKey Key { get; set; }
-    public TValue Value { get; set; }
-
-    public CacheItem(TKey key, TValue value)
-    {
-        Key = key;
-        Value = value;
-    }
+    public TKey Key { get; } = key;
+    public TValue Value { get; set; } = value;
 }
