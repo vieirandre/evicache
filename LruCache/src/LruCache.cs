b9bb57448e089dadc8caa486843ceff13ee68fd9
vieirandre <andre.vieira@outlook.com>
2025-01-25 02:01:14 -0300

    Declare & init _cacheMap and _lruList fields

--- LruCache/src/LruCache.cs
+++ LruCache/src/LruCache.cs
@@ -1,14 +1,20 @@
-﻿namespace LruCache;
+﻿using LruCache.Models;
+
+namespace LruCache;
 
 public class LruCache<TKey, TValue> : ILruCache<TKey, TValue> where TKey : notnull
 {
     private readonly int _capacity;
+    private readonly Dictionary<TKey, LinkedListNode<CacheItem<TKey, TValue>>> _cacheMap;
+    private readonly LinkedList<CacheItem<TKey, TValue>> _lruList;
 
     public LruCache(int capacity)
     {
         ArgumentOutOfRangeException.ThrowIfNegativeOrZero(capacity);
 
         _capacity = capacity;
+        _cacheMap = new Dictionary<TKey, LinkedListNode<CacheItem<TKey, TValue>>>(capacity);
+        _lruList = new LinkedList<CacheItem<TKey, TValue>>();
     }
 
     public TValue Get(TKey key)
