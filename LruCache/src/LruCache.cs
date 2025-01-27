525182be2040065e446ec02bee466197ab55036a
vieirandre <andre.vieira@outlook.com>
2025-01-27 01:19:02 -0300

    Expose readonly count property for cache

--- LruCache/src/LruCache.cs
+++ LruCache/src/LruCache.cs
@@ -8,6 +8,8 @@ public class LruCache<TKey, TValue> : ILruCache<TKey, TValue> where TKey : notnu
     private readonly Dictionary<TKey, LinkedListNode<CacheItem<TKey, TValue>>> _cacheMap;
     private readonly LinkedList<CacheItem<TKey, TValue>> _lruList;
 
+    public int Count => _cacheMap.Count;
+
     public LruCache(int capacity)
     {
         ArgumentOutOfRangeException.ThrowIfNegativeOrZero(capacity);
