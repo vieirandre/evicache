2d6984b735258a0fe4640e986c874be0f793ef7a
vieirandre <andre.vieira@outlook.com>
2025-01-26 22:12:58 -0300

    Create test for eviction

--- LruCache/tests/LruCacheTests.cs
+++ LruCache/tests/LruCacheTests.cs
@@ -18,4 +18,28 @@ public class LruCacheTests
 
         Assert.Equal("value", result);
     }
+
+    [Fact]
+    public void Test2()
+    {
+        // arrange
+
+        var cache = new LruCache<int, string>(2);
+
+        cache.Put(1, "valueOne");
+        cache.Put(2, "valueTwo");
+
+        // act
+
+        cache.TryGet(1, out var valueOne);
+        cache.Put(3, "valueThree");
+        cache.TryGet(2, out var valueTwo);
+        cache.TryGet(3, out var valueThree);
+
+        // assert
+
+        Assert.Null(valueTwo);
+        Assert.Equal("valueOne", valueOne);
+        Assert.Equal("valueThree", valueThree);
+    }
 }
