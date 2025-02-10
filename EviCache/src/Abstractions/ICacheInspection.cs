92b126dd147361b06c6c770b0640bee9179729eb
vieirandre <andre.vieira@outlook.com>
2025-02-10 06:20:23 -0300

    Add inspection interface

---
+++ EviCache/src/Abstractions/ICacheInspection.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Immutable;
+
+namespace EviCache.Abstractions;
+
+public interface ICacheInspection<TKey> where TKey : notnull
+{
+    ImmutableList<TKey> GetKeysInOrder();
+}
