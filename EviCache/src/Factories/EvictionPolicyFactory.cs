53cbf97089c5ae2801b5bd1e06cd2e58bf19e095
vieirandre <andre.vieira@outlook.com>
2025-02-10 06:23:49 -0300

    Add evict policy factory

---
+++ EviCache/src/Factories/EvictionPolicyFactory.cs
@@ -0,0 +1,17 @@
+﻿using EviCache.Abstractions;
+using EviCache.Enums;
+using EviCache.Policies;
+
+namespace EviCache.Factories;
+
+public static class EvictionPolicyFactory
+{
+    public static IEvictionPolicy<TKey, TValue> Create<TKey, TValue>(EvictionPolicyType policyType) where TKey : notnull
+    {
+        return policyType switch
+        {
+            EvictionPolicyType.LRU => new LruEvictionPolicy<TKey, TValue>(),
+            _ => throw new NotSupportedException($"The eviction policy '{policyType}' is not supported")
+        };
+    }
+}
