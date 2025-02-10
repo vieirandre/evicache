e55d5b19d58b5366747dfe4a15053484893c5c24
vieirandre <andre.vieira@outlook.com>
2025-02-10 02:20:09 -0300

    Remove cache item class

--- EviCache/src/Models/CacheItem.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EviCache.Models;
-
-public class CacheItem<TKey, TValue>(TKey key, TValue value)
-{
-    public TKey Key { get; } = key;
-    public TValue Value { get; set; } = value;
-}
