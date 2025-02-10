73bd9b66edf8244446a9429b718606b11b26dd09
vieirandre <andre.vieira@outlook.com>
2025-02-10 06:22:11 -0300

    Modify signature of eviction candidate method to 'try' style

--- EviCache/src/Abstractions/IEvictionPolicy.cs
+++ EviCache/src/Abstractions/IEvictionPolicy.cs
@@ -1,11 +1,11 @@
 ﻿namespace EviCache.Abstractions;
 
-public interface IEvictionPolicy<TKey, TValue> where TKey : notnull
+public interface IEvictionPolicy<TKey, TValue> : ICacheInspection<TKey> where TKey : notnull
 {
     void RecordAccess(TKey key);
     void RecordInsertion(TKey key);
     void RecordUpdate(TKey key);
     void RecordRemoval(TKey key);
     void Clear();
-    TKey? SelectEvictionCandidate();
+    bool TrySelectEvictionCandidate(out TKey candidate);
 }
