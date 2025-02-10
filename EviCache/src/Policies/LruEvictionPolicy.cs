ac09559cbf1d35f503e026e93ae8f7501ece2249
vieirandre <andre.vieira@outlook.com>
2025-02-10 06:23:15 -0300

    Create impl of LRU policy

---
+++ EviCache/src/Policies/LruEvictionPolicy.cs
@@ -0,0 +1,50 @@
+﻿using EviCache.Abstractions;
+using System.Collections.Immutable;
+
+namespace EviCache.Policies;
+
+public class LruEvictionPolicy<TKey, TValue> : IEvictionPolicy<TKey, TValue> where TKey : notnull
+{
+    private readonly LinkedList<TKey> _cacheList = new();
+
+    public void RecordAccess(TKey key)
+    {
+        _cacheList.Remove(key);
+        _cacheList.AddFirst(key);
+    }
+
+    public void RecordInsertion(TKey key)
+    {
+        _cacheList.AddFirst(key);
+    }
+
+    public void RecordUpdate(TKey key)
+    {
+        RecordAccess(key);
+    }
+
+    public void RecordRemoval(TKey key)
+    {
+        _cacheList.Remove(key);
+    }
+
+    public void Clear()
+    {
+        _cacheList.Clear();
+    }
+
+    public bool TrySelectEvictionCandidate(out TKey candidate)
+    {
+        if (_cacheList.Last is null)
+        {
+            candidate = default!;
+            return false;
+        }
+
+        candidate = _cacheList.Last.Value;
+        return true;
+    }
+
+    public ImmutableList<TKey> GetKeysInOrder()
+        => _cacheList.ToImmutableList();
+}
