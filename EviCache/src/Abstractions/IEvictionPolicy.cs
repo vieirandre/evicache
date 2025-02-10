021bf31c98fcfee4d95ff76ee544f05560e5af85
vieirandre <andre.vieira@outlook.com>
2025-02-10 01:03:00 -0300

    Create interface for eviction policy

---
+++ EviCache/src/Abstractions/IEvictionPolicy.cs
@@ -0,0 +1,11 @@
+﻿namespace EviCache.Abstractions;
+
+public interface IEvictionPolicy<TKey, TValue> where TKey : notnull
+{
+    void RecordAccess(TKey key);
+    void RecordInsertion(TKey key);
+    void RecordUpdate(TKey key);
+    void RecordRemoval(TKey key);
+    void Clear();
+    TKey? SelectEvictionCandidate();
+}
