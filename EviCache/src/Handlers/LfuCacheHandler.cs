bd0629900ecf99db1c50e73dbe8c070c4cd3f631
vieirandre <andre.vieira@outlook.com>
2025-02-13 03:19:40 -0300

    Draft LFU handler

---
+++ EviCache/src/Handlers/LfuCacheHandler.cs
@@ -0,0 +1,39 @@
+﻿using EviCache.Abstractions;
+using System.Collections.Immutable;
+
+namespace EviCache.Handlers;
+
+public class LfuCacheHandler<TKey, TValue> : ICacheHandler<TKey, TValue> where TKey : notnull
+{
+    public ImmutableList<TKey> InternalCollection => throw new NotImplementedException();
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RecordAccess(TKey key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RecordInsertion(TKey key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RecordRemoval(TKey key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RecordUpdate(TKey key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TrySelectEvictionCandidate(out TKey candidate)
+    {
+        throw new NotImplementedException();
+    }
+}
