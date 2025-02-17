7cfa183d95d5fa5c421f5e2720635e6663ad824b
vieirandre <andre.vieira@outlook.com>
2025-02-17 05:59:16 -0400

    Rename main cache class

--- EviCache/src/Cache.Core.cs
+++ EviCache/src/Cache.Core.cs
@@ -4,14 +4,14 @@ using EviCache.Factories;
 
 namespace EviCache;
 
-public partial class EviCache<TKey, TValue> : ICacheOperations<TKey, TValue>, ICacheMetrics, ICacheInspection<TKey, TValue>, IDisposable where TKey : notnull
+public partial class Cache<TKey, TValue> : ICacheOperations<TKey, TValue>, ICacheMetrics, ICacheInspection<TKey, TValue>, IDisposable where TKey : notnull
 {
     private readonly int _capacity;
     private readonly Dictionary<TKey, TValue> _cacheMap;
     private readonly ICacheHandler<TKey, TValue> _cacheHandler;
     private readonly object _syncLock = new();
 
-    public EviCache(int capacity, EvictionPolicy evictionPolicy)
+    public Cache(int capacity, EvictionPolicy evictionPolicy)
     {
         if (capacity <= 0) throw new ArgumentOutOfRangeException(nameof(capacity));
 
--- EviCache/src/Cache.Disposal.cs
+++ EviCache/src/Cache.Disposal.cs
@@ -2,7 +2,7 @@
 
 namespace EviCache;
 
-public partial class EviCache<TKey, TValue> : ICacheOperations<TKey, TValue>, ICacheMetrics, ICacheInspection<TKey, TValue>, IDisposable where TKey : notnull
+public partial class Cache<TKey, TValue> : ICacheOperations<TKey, TValue>, ICacheMetrics, ICacheInspection<TKey, TValue>, IDisposable where TKey : notnull
 {
     public void Clear()
     {
