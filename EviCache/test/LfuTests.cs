32822f0f9c4a08d64746ec21e002a55f34a07774
vieirandre <andre.vieira@outlook.com>
2025-02-20 21:19:18 -0400

    Test LFU eviction

--- EviCache/test/LfuTests.cs
+++ EviCache/test/LfuTests.cs
@@ -25,4 +25,31 @@ public class LfuTests
         Assert.True(found);
         Assert.Equal("value", result);
     }
+
+    [Fact]
+    public void Should_EvictLeastFrequentlyUsed_WhenCapacityIsExceeded()
+    {
+        // arrange
+
+        var options = new CacheOptions(2, _evictionPolicy);
+        var cache = new Cache<int, string>(options);
+
+        cache.Put(1, "value1");
+        cache.Put(2, "value2");
+
+        cache.Get(1);
+        cache.Get(1);
+
+        // act
+
+        cache.Put(3, "value3");
+
+        // assert
+
+        Assert.False(cache.TryGet(2, out _));
+        Assert.True(cache.TryGet(1, out var value1));
+        Assert.Equal("value1", value1);
+        Assert.True(cache.TryGet(3, out var value3));
+        Assert.Equal("value3", value3);
+    }
 }
