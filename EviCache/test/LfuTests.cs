4788242d39fe33e4480aeb3164f374d1e6d732ce
vieirandre <andre.vieira@outlook.com>
2025-02-20 21:26:27 -0400

    Add two common tests

--- EviCache/test/LfuTests.cs
+++ EviCache/test/LfuTests.cs
@@ -52,4 +52,47 @@ public class LfuTests
         Assert.True(cache.TryGet(3, out var value3));
         Assert.Equal("value3", value3);
     }
+
+    [Fact]
+    public void Should_StoreAndRetrieve_WhenKeyIsStringAndValueIsInt()
+    {
+        // arrange
+
+        var options = new CacheOptions(2, _evictionPolicy);
+        var cache = new Cache<string, int>(options);
+
+        // act
+
+        cache.Put("this", 10);
+        cache.Put("that", 20);
+        bool found = cache.TryGet("that", out var thatValue);
+
+        // assert
+
+        Assert.True(found);
+        Assert.Equal(20, thatValue);
+    }
+
+    [Fact]
+    public void Should_UpdateExistingKeyAndEvictLeastFrequentlyUsed_WhenReinsertingKeyOverCapacity()
+    {
+        // arrange
+
+        var options = new CacheOptions(2, _evictionPolicy);
+        var cache = new Cache<int, string>(options);
+
+        cache.Put(1, "oldValue");
+        cache.Put(2, "value2");
+
+        // act
+
+        cache.Put(1, "newValue");
+        cache.Put(3, "value3");
+
+        // assert
+
+        Assert.False(cache.TryGet(2, out _));
+        Assert.Equal("newValue", cache.Get(1));
+        Assert.Equal("value3", cache.Get(3));
+    }
 }
