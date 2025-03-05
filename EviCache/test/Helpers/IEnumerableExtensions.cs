10ebfe0d4cc5ae4cafda7111ca1e089e68e27d3c
vieirandre <andre.vieira@outlook.com>
2025-03-05 19:37:50 -0300

    Create ext method for Fisher-Yates shuffle

---
+++ EviCache/test/Helpers/IEnumerableExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace EviCache.Tests.Helpers;
+
+public static class IEnumerableExtensions
+{
+    public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+    {
+        if (source is null) throw new ArgumentNullException(nameof(source));
+
+        var list = source.ToList();
+        var rng = new Random();
+        int n = list.Count;
+
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            (list[k], list[n]) = (list[n], list[k]);
+        }
+
+        foreach (var item in list)
+        {
+            yield return item;
+        }
+    }
+}
