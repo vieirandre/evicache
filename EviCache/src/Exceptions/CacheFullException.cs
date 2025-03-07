de6077249907fe64f7901cfd303c9278a30b116a
vieirandre <andre.vieira@outlook.com>
2025-03-07 01:31:12 -0300

    Add cache full exception

---
+++ EviCache/src/Exceptions/CacheFullException.cs
@@ -0,0 +1,16 @@
+﻿namespace EviCache.Exceptions;
+
+public class CacheFullException : Exception
+{
+    public int Capacity { get; }
+
+    public CacheFullException(string message) : base(message)
+    {
+        Capacity = -1;
+    }
+
+    public CacheFullException(string message, int capacity) : base(message)
+    {
+        Capacity = capacity;
+    }
+}
