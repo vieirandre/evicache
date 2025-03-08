de3992da2254239fe8ad015efad445fb1d4e33ff
vieirandre <andre.vieira@outlook.com>
2025-03-08 05:43:32 -0300

    Test LFU eviction failures

--- EviCache/test/LfuTests.cs
+++ EviCache/test/LfuTests.cs
@@ -1,4 +1,5 @@
 ﻿using EviCache.Enums;
+using EviCache.Exceptions;
 using EviCache.Tests.Helpers;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -224,5 +225,47 @@ public class LfuTests : CacheTestsBase
         Assert.False(cache.TryGet(1, out _));
         Assert.True(cache.TryGet(11, out var value11));
         Assert.Equal("value11", value11);
+    }
+
+    [Fact]
+    public void Should_ThrowException_WhenEvictionFails_DueToNoCandidate()
+    {
+        // arrange
+
+        var cache = CreateCache<int, string>(1, _loggerMock.Object);
+        cache.Put(1, "value1");
+
+        cache.OverrideEvictionCandidateCollection("_frequencyBuckets", new SortedDictionary<int, LinkedList<int>>());
+
+        // act & assert
+
+        var ex = Assert.Throws<CacheFullException>(() => cache.Put(2, "value2"));
+
+        Assert.Equal("Cache is full (capacity: 1) and eviction failed for key: 2", ex.Message);
+        _loggerMock.VerifyLog(LogLevel.Error, "Eviction selector did not return a candidate", Times.Once());
+    }
+
+    [Fact]
+    public void Should_ThrowException_WhenEvictionFails_DueToCandidateNotInCache()
+    {
+        // arrange
+
+        var cache = CreateCache<int, string>(1, _loggerMock.Object);
+        cache.Put(1, "value1");
+
+        var fakeFrequencyBuckets = new SortedDictionary<int, LinkedList<int>>();
+        var fakeCandidateList = new LinkedList<int>();
+        int fakeCandidate = 999;
+        fakeCandidateList.AddLast(fakeCandidate);
+        fakeFrequencyBuckets.Add(1, fakeCandidateList);
+
+        cache.OverrideEvictionCandidateCollection("_frequencyBuckets", fakeFrequencyBuckets);
+
+        // act & assert
+
+        var ex = Assert.Throws<CacheFullException>(() => cache.Put(2, "value2"));
+
+        Assert.Equal("Cache is full (capacity: 1) and eviction failed for key: 2", ex.Message);
+        _loggerMock.VerifyLog(LogLevel.Error, $"Eviction candidate ({fakeCandidate}) was not found in the cache", Times.Once());
     }
 }
