d4fc5993e30ef2de061489ecda0eba4835d9b389
vieirandre <andre.vieira@outlook.com>
2025-03-08 05:42:21 -0300

    Refactor reflection logic for eviction override into ext method

---
+++ EviCache/test/Helpers/CacheExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace EviCache.Tests.Helpers;
+
+public static class CacheExtensions
+{
+    public static void OverrideEvictionCandidateCollection<TKey, TValue>(this Cache<TKey, TValue> cache, string candidateCollectionFieldName, object fakeCandidateCollection) where TKey : notnull
+    {
+        var selectorField = typeof(Cache<TKey, TValue>).GetField("_evictionCandidateSelector", BindingFlags.Instance | BindingFlags.NonPublic);
+        Assert.NotNull(selectorField);
+
+        var evictionHandler = selectorField.GetValue(cache);
+        Assert.NotNull(evictionHandler);
+
+        var handlerCollectionField = evictionHandler.GetType().GetField(candidateCollectionFieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+        Assert.NotNull(handlerCollectionField);
+
+        handlerCollectionField.SetValue(evictionHandler, fakeCandidateCollection);
+    }
+}
