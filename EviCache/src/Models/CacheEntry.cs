2d39fe847e9b33d7915eefb5e25c7fb403f6bed0
vieirandre <andre.vieira@outlook.com>
2025-04-20 02:39:34 -0300

    Create cache entry

---
+++ EviCache/src/Models/CacheEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace EviCache.Models;
+
+internal class CacheEntry<TValue>
+{
+    public TValue Value { get; }
+    public CacheEntryMetadata Metadata { get; }
+
+    public CacheEntry(TValue value)
+    {
+        Value = value;
+        Metadata = new CacheEntryMetadata();
+    }
+}
