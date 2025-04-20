be9e82499c25612f0c36ec7e1a1ade4495677847
vieirandre <andre.vieira@outlook.com>
2025-04-20 02:31:05 -0300

    Set ctor & access count for metadata

--- EviCache/src/Models/CacheEntryMetadata.cs
+++ EviCache/src/Models/CacheEntryMetadata.cs
@@ -5,4 +5,16 @@ internal class CacheEntryMetadata
     public DateTimeOffset CreatedAt { get; }
     public DateTimeOffset LastAccessedAt { get; private set; }
     public DateTimeOffset LastUpdatedAt { get; private set; }
+    public long AccessCount => Interlocked.Read(ref _accessCount);
+
+    private long _accessCount;
+
+    internal CacheEntryMetadata()
+    {
+        var now = DateTimeOffset.UtcNow;
+        CreatedAt = now;
+        LastAccessedAt = now;
+        LastUpdatedAt = now;
+        _accessCount = 0;
+    }
 }
