c4b9f4d2938d75b2eccef57b8e10aab16c25e669
vieirandre <andre.vieira@outlook.com>
2025-04-20 02:25:04 -0300

    Draft metadata class

---
+++ EviCache/src/Models/CacheEntryMetadata.cs
@@ -0,0 +1,8 @@
+﻿namespace EviCache.Models;
+
+internal class CacheEntryMetadata
+{
+    public DateTimeOffset CreatedAt { get; }
+    public DateTimeOffset LastAccessedAt { get; private set; }
+    public DateTimeOffset LastUpdatedAt { get; private set; }
+}
