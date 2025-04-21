7cb9a8bba3ac55764e69806ea8113408f984a7c9
vieirandre <andre.vieira@outlook.com>
2025-04-21 02:14:12 -0300

    Add xml comments for metadata stuff

--- EviCache/src/Abstractions/ICacheMetadata.cs
+++ EviCache/src/Abstractions/ICacheMetadata.cs
@@ -2,8 +2,25 @@
 
 namespace EviCache.Abstractions;
 
+/// <summary>
+/// Provides methods for accessing cache item metadata.
+/// </summary>
+/// <typeparam name="TKey">The type of keys in the cache.</typeparam>
 public interface ICacheMetadata<TKey> where TKey : notnull
 {
+    /// <summary>
+    /// Gets the metadata for the specified cache item.
+    /// </summary>
+    /// <param name="key">The key of the cache item.</param>
+    /// <returns>The metadata for the specified item.</returns>
+    /// <exception cref="KeyNotFoundException">Thrown if the key is not found in the cache.</exception>
     CacheItemMetadata GetMetadata(TKey key);
+
+    /// <summary>
+    /// Attempts to get the metadata for the specified cache item.
+    /// </summary>
+    /// <param name="key">The key of the cache item.</param>
+    /// <param name="metadata">When this method returns, contains the metadata associated with the key, if found; otherwise, null.</param>
+    /// <returns>true if the metadata was found; otherwise, false.</returns>
     bool TryGetMetadata(TKey key, out CacheItemMetadata? metadata);
 }
--- EviCache/src/Models/CacheItemMetadata.cs
+++ EviCache/src/Models/CacheItemMetadata.cs
@@ -1,10 +1,28 @@
 ﻿namespace EviCache.Models;
 
+/// <summary>
+/// Represents metadata for a cache item.
+/// </summary>
 public class CacheItemMetadata
 {
+    /// <summary>
+    /// Gets the time when the cache item was created.
+    /// </summary>
     public DateTimeOffset CreatedAt { get; }
+
+    /// <summary>
+    /// Gets the time when the cache item was last accessed.
+    /// </summary>
     public DateTimeOffset LastAccessedAt { get; private set; }
+
+    /// <summary>
+    /// Gets the time when the cache item was last updated.
+    /// </summary>
     public DateTimeOffset LastUpdatedAt { get; private set; }
+
+    /// <summary>
+    /// Gets the number of times this item has been accessed.
+    /// </summary>
     public long AccessCount => Interlocked.Read(ref _accessCount);
 
     private long _accessCount;
